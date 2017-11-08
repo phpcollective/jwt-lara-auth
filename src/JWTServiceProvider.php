1731f3b150ad635c97cb6a7f2725931e45970bf2
Al Amin Chayan <chayan.php@gmail.com>
2017-11-08 14:45:00 +0600

    first commit

---
+++ src/JWTServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PHPCollective\JWTLaraAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+class JWTServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
